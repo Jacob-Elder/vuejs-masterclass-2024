d14accae5c7617371a1d6543eae84b418722654d
Jacob-Elder <jacobelderonly@gmail.com>
2025-05-11 03:19:37 -0700

    auto routing and supabase

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { supabase } from '@/lib/supabaseClient'
+
+console.log('supabase : ', supabase)
+</script>
 
 <template>
-  <h1>Welcome To My App</h1>
-  <!-- <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView /> -->
+  <main>
+    <RouterView />
+  </main>
 </template>
---
+++ src/pages/projects/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1>Projects</h1>
+    <ul>
+      <li>Vue JS Masterclass</li>
+      <li>Mastering Nuxt</li>
+      <li>Git Basics</li>
+    </ul>
+    <RouterLink to="/">Go Home</RouterLink>
+  </div>
+</template>
